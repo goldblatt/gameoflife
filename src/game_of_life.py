bd1b14b3569b4eca679b020cc026ae150c44ad5a
Susan Goldblatt <sgoldblatt@google.com>
2022-04-14 15:25:24 -0700

    parsing and to_string

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,8 +1,24 @@
 #! /usr/bin/env python3
 
 class GameOfLife():
-    def run(self):
-        print("I don't do much, yet.")
+    def run(self, input):
+        parsed_input = self.parse(input)
+        print(self.to_string(parsed_input))
+    
+    def to_string(self, cells_array):
+        output_string = ""
+        for cell in cells_array:
+            output_string += str(cell[0]) + "," + str(cell[1]) + " "
+        return output_string.strip()
+    
+    def parse(self, input):
+        pairs = input.split(" ")
+        cells_array = []
+        for pair in pairs:
+            split_pair = pair.split(",")
+            cells_array.append([int(split_pair[0]), int(split_pair[1])])
+        return cells_array
+
 
 if __name__ == "__main__":
     GameOfLife().run()
--- test/test_game_of_life.py
+++ test/test_game_of_life.py
@@ -5,25 +5,12 @@ import contextlib
 from src.game_of_life import GameOfLife
 
 class GameOfLifeTest(unittest.TestCase):
-
-    def test_run_doesnt_do_much(self):
-        game = GameOfLife()
-        fake_stdout = io.StringIO()
-
-        with contextlib.redirect_stdout(fake_stdout):
-            game.run()
-
-        output = fake_stdout.getvalue()
-        fake_stdout.close()
-
-        self.assertEqual(output, "I don't do much, yet.\n")
-
     def test_still_life_block(self):
         game = GameOfLife()
         fake_stdout = io.StringIO()
 
         input = "1,1 1,2 2,1 2,2"
-        expected = "1,1 1,2 2,1 2,2"
+        expected = "1,1 1,2 2,1 2,2\n"
 
         with contextlib.redirect_stdout(fake_stdout):
             game.run(input)
@@ -38,7 +25,7 @@ class GameOfLifeTest(unittest.TestCase):
         fake_stdout = io.StringIO()
 
         input = "1,2 2,2 3,2"
-        expected = "2,1 2,2 2,3"
+        expected = "2,1 2,2 2,3\n"
 
         with contextlib.redirect_stdout(fake_stdout):
             game.run(input)
