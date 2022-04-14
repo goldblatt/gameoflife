8a5fadab7f387bdb057e11d20c86bc438db7dfb6
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-04-14 14:19:08 -0700

    Adding tests for blinker and block

--- test/test_game_of_life.py
+++ test/test_game_of_life.py
@@ -17,3 +17,40 @@ class GameOfLifeTest(unittest.TestCase):
         fake_stdout.close()
 
         self.assertEqual(output, "I don't do much, yet.\n")
+
+    def test_still_life_block(self):
+        game = GameOfLife()
+        fake_stdout = io.StringIO()
+
+        input = "1,1 1,2 2,1 2,2"
+        expected = "1,1 1,2 2,1 2,2"
+
+        with contextlib.redirect_stdout(fake_stdout):
+            game.run(input)
+
+        output = fake_stdout.getvalue()
+        fake_stdout.close()
+
+        self.assertEqual(output, expected)
+
+    def test_oscillator_blinker(self):
+        game = GameOfLife()
+        fake_stdout = io.StringIO()
+
+        input = "1,2 2,2 3,2"
+        expected = "2,1 2,2 2,3"
+
+        with contextlib.redirect_stdout(fake_stdout):
+            game.run(input)
+
+        output = fake_stdout.getvalue()
+        fake_stdout.close()
+
+        self.assertEqual(output, expected)
+
+
+    #def test_still_life_block(self):
+        # 1,1 1,2 2,1 2,2
+        # 1,2 2,1 2,3  3,1 3,3 4,2 
+
+    #    self.assertEqual(True, False)
